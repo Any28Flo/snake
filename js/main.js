c180238ea75399dfb6877c1160ee469069751fd0
“Erika <foglzerika@gmail.com>
2017-04-09 19:22:19 -0500

    Agregando addEventListener a las letras

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+var evento1 = document.getElementById('lienzo');
+evento1.addEventListener("keydown", direcciones);
 
+function presionaTeclas(event){
+  console.log(event.keyCode);
+}
+
+function direcciones(event){
+  if(event.keyCode==39){
+    console.log("se movio a la derecha");
+  }else if (event.keyCode==37) {
+    console.log("Se movio a la izquierda");
+
+  }else if (event.keyCode==40) {
+    console.log("se movio hacia abajo");
+
+  }else if (event.keyCode==38) {
+    console.log("Se movio hacia ariba ");
+  }else {
+    alert("Tecla equivocad")
+  }
+
+}
