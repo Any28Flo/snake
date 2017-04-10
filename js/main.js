2b4e66f8aabdf5f132f6c8b3ace7e2dc2727a229
“Erika <foglzerika@gmail.com>
2017-04-09 21:42:05 -0500

    Agregando cuadro snake

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var evento1 = document.getElementById('lienzo');
-evento1.addEventListener("keydown", direcciones);
+//evento1.addEventListener("keydown", presionaTeclas);
+evento1.addEventListener("keydown",direcciones);
+//console.log(evento1) ;
+var figuraSnake = document.getElementById("snake");
+//console.log(figuraSnake);
+//figuraSnake.addEventListener("keydown", mueveSnake);
+
+
 
 function presionaTeclas(event){
-  console.log(event.keyCode);
+  console.log(event);
 }
 
 function direcciones(event){
   if(event.keyCode==39){
+    console.log(figuraSnake.style);
+  //  figuraSnake.screenX +=10px;
+
     console.log("se movio a la derecha");
   }else if (event.keyCode==37) {
     console.log("Se movio a la izquierda");
@@ -17,7 +27,12 @@ function direcciones(event){
   }else if (event.keyCode==38) {
     console.log("Se movio hacia ariba ");
   }else {
-    alert("Tecla equivocad")
+    alert("Tecla equivoda")
   }
 
+}
+function mueveSnake(event){
+  if(event.keyCode==39){
+
+  }
 }
